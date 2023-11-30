95bd50aa1509cc8c365b7b99d8a725740900087c
Colin Harrington <colin.harrington@gmail.com>
2023-11-30 03:07:04 -0600

    Day 24: Air Duct Spelunking - The problem starts as a maze/pathfinding problem. I built a graph of the maze, then used an A* algorithm to find the shortest paths between points of interest. - Once I had the steps between waypoints, I went through the permutations and found the fewest steps starting at 0 - Part two was simple to add - Just adding the return trip to each of the scenarios and finding the fewest steps. The last part of this problem looks kinda like a travelling salesman problem, but with only 8 waypoints, the brute force - checking each scenario worked in ~20ms
    ```
    🎄 Part 1 🎄
    448 (elapsed: 23.78ms)
    🎄 Part 2 🎄
    672 (elapsed: 22.12ms)
    ```

--- src/bin/24.rs
+++ src/bin/24.rs
@@ -165,6 +165,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 24);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(20));
     }
 }
